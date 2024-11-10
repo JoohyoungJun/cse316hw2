fd3cd26750dc5e5912f6fff8b14083959b9d68d0
JoohyoungJun <joohyoung.jun@stonybrook.edu>
2024-11-10 19:20:37 +0900

    Fourth commit: modified sql file with two tables

--- db/reservation_db.sql
+++ db/reservation_db.sql
@@ -2,13 +2,28 @@ CREATE DATABASE reservation_db;
 
 USE reservation_db;
 
-CREATE TABLE reservations (
-    --id, facility, image, date, location, numPeople, affiliation, purpose
-    id INT NOT NULL,
-    facility VARCHAR(100) NOT NULL,
-    date Date NOT NULL,
-    numPeople INT NOT NULL,
-    affiliation VARCHAR(100) NOT NULL,
-    purpose VARCHAR(100) NOT NULL
+CREATE TABLE facilities (
+    --id, facilityName, facilityDscr, imageSrc, availableDays, minCap, maxCap, 
+    --location, onlySKFlag
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    facilityName VARCHAR(100) NOT NULL,
+    facilityDscr TEXT, 
+    imageSrc VARCHAR(255), 
+    availableDays VARCHAR(100),
+    minCap INT,
+    maxCap INT,
+    location VARCHAR(100),
+    onlySKFlag BOOLEAN
+);
 
+CREATE TABLE reservations(
+    --reservationId, reservationDate, userNum, isSK
+    --purpose, reservationName, userName
+    reservationId INT NOT NULL,
+    reservationDate DATE,
+    userNum INT,
+    isSK BOOLEAN,
+    purpose VARCHAR(255),
+    reservationName VARCHAR(100),
+    userName VARCHAR(100)
 );
